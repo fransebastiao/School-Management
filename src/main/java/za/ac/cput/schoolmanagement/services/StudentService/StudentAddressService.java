28e8c9cedf1d813d2ea5e2e2e7a0a3596cabd2e9
Edvalter Jamba <edvalterjamba9@gmail.com>
2022-06-18 02:43:39 +0200

    Edvalter_Jamba_220446571 Service and ServiceAddress and tests Case

---
+++ src/main/java/za/ac/cput/schoolmanagement/services/StudentService/StudentAddressService.java
@@ -0,0 +1,14 @@
+package za.ac.cput.schoolmanagement.services.StudentService;
+/*
+ School_Management
+ StudentAddressService
+ Author: Edvalter da Costa Jamba (220446571)
+ Date: 16 June 2022
+*/
+import za.ac.cput.schoolmanagement.domain.StudentAddress;
+import za.ac.cput.schoolmanagement.services.IService;
+
+public interface StudentAddressService extends IService<StudentAddress,String>
+{
+        void deleteUsingId(String id);
+}
---
+++ src/main/java/za/ac/cput/schoolmanagement/services/StudentService/StudentServiceImpl.java
@@ -0,0 +1,54 @@
+/*
+ School_Management
+ StudentService
+ Author: Edvalter da Costa Jamba (220446571)
+ Date: 16 June 2022
+*/
+
+package za.ac.cput.schoolmanagement.services.StudentService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.schoolmanagement.domain.Student;
+import za.ac.cput.schoolmanagement.repository.StudentRepository;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository repository;
+
+    @Autowired public StudentServiceImpl(StudentRepository repository){
+        this.repository = repository;
+    }
+
+    @Override
+    public Student save(Student student)
+    {
+        return this.repository.save(student);
+    }
+    @Override
+    public Optional<Student> read(String id) {
+        return this.repository.findById(id);
+    }
+
+    @Override
+    public void delete(Student student) {
+        this.repository.delete(student);
+    }
+
+
+    @Override
+    public List<Student> findAll() {
+        return this.repository.findAll();
+    }
+
+    public void deleteUsingId(String id) {
+        repository.deleteById(id);
+        Optional<Student> student = read(id);
+        if (student.isPresent()) {
+            delete(student.get());
+        }
+    }
+}
